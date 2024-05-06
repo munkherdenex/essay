dde8ac23628153432248e2d3799fb467db06e26c
munkherdenex <munkherdenex@gmail.com>
2024-05-06 23:12:44 +0800

    feat: added a "go back" button to "az jargal" essay

--- search.js
+++ search.js
@@ -1,6 +1,4 @@
-// search.js
 
-// Function to perform search as you type
 document.getElementById('searchInput').addEventListener('input', function() {
     var searchText = this.value.toLowerCase(); // Get the search query
     var articles = document.querySelectorAll('#essays article'); // Get all articles
